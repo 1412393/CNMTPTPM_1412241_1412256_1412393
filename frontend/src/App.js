72c4dbc49bc92fd6c80c29c80a1c567c5e853c2e
DELL\admin <khanhhaquang@gmail.com>
2017-12-31 18:34:47 +0700

    init redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import {Provider} from 'react-redux';
+import store from './store.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Provider store={store}>
+      <Switch>
+        <Route exact path='/' component={}/>
+        <Route path='/signup' component={}/>
+      </Switch>
+      </Provider>
     );
   }
 }
