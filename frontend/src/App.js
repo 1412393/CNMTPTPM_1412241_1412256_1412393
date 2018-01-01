4abf506c990f6bb74477408f1a7a80f113cfc002
DELL\admin <khanhhaquang@gmail.com>
2018-01-01 21:41:54 +0700

    add login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store.js';
+import SignIn from './components/SigninForm.js';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <Switch>
-        <Route exact path='/' component={}/>
-        <Route path='/signup' component={}/>
-      </Switch>
+        <Switch>
+          <Route exact path='/' component={SignIn}/>
+          <Route path='/signup' component={SignIn}/>
+        </Switch>
       </Provider>
-    );
+    )
   }
 }
 
--- frontend/src/components/SigninForm.js
+++ frontend/src/components/SigninForm.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import * as actions from '../actions/SigninActions.js'
 
 class SignIn extends Component {
+
+
   render() {
     return (
-      <div className="App">
-
+      <div className= "login-form">
+            <h2>Login</h2>
+            <div>
+                <input ref="email" type="email" name="e" placeholder="Email" required="required" />
+                <input ref="password" type="password" name="p" placeholder="Password" required="required" />
+                <button onClick={this.handleLogin} className="btn btn-primary btn-block btn-large">Let me in.</button>
+            </div>
       </div>
     );
   }
 }
+
+export default SignIn;
