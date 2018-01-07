7a65603c9513ebe42b63a76fb4125fd50f173172
1412393 <1412393@student.hcmus.edu.vn>
2018-01-07 11:41:05 +0700

    create block, transaction model

---
+++ backend/models/log.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var validate = require('mongoose-validator'); // Import Mongoose Validator Plugin
+var passportLocalMongoose = require('passport-local-mongoose');
+
+
+
+
+/*var UserSchema = Schema({
+    password: { type: String },
+    email: { type: String },
+    coins: { type: String }
+});*/
+
+
+var UserSchema = Schema({
+    name: String,
+    email: { type: String, unique: true },
+    roles: [{ type: 'String' }],
+    isVerified: { type: Boolean, default: false },
+    password: String,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    address:{ address: String, privateKey: String, publicKey:String },
+    available_coins: Number,
+    actual_coins: Number
+});
+
+/*UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) { return cb(err); }
+
+        cb(null, isMatch);
+    });
+};*/
+//Export model
+if (mongoose.models.User) {
+    module.exports = mongoose.model('User');
+} else {
+    module.exports = mongoose.model('User', UserSchema);
+}
+
+
